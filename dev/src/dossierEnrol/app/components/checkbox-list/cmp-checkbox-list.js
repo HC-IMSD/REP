b21933b2afdc568bac9d74f223c4a6dac14f6d66
achtatou <achtatou@softpoint.ca>
2016-08-11 23:15:01 -0400

    Generic check box list component

--- dev/src/dossierEnrol/app/components/checkbox-list/cmp-checkbox-list.js
+++ dev/src/dossierEnrol/app/components/checkbox-list/cmp-checkbox-list.js
@@ -21,14 +21,48 @@
             controllerAs: 'chkl',
             bindings: {
                 title: '@',
-                listItems: '<'
-                //onUpdateDossier: '&',
-               //onDeleteDossier: '&'
+                listItems: '<' //array of objects
             }
         });
 
 
     function checkBoxListCtrl(){
+
+        var self = this;
+
+        self.$onInit = function(){
+
+            var  object= {};
+
+            for(var item in self.listItems){
+                object[item.name] = item.value;
+            }
+
+            self.roleModel = JSON.stringify(object);
+
+        }
+
+        self.someSelected = function () {
+            var object = self.roleModel;
+
+            if (!object) return false;
+            return Object.keys(object).some(function (key) {
+                //console.log("cmpAddressRole someSelected: " + object[key]);
+                return object[key];
+            });
+        }
+
+        self.updateRoleModel = function () {
+
+            self.formName.addressRole.$dirty =
+                self.formName.addressRole.$touched = true;
+
+            self.formName.addressRole.$pristine = !self.formName.addressRole.$dirty;
+            self.formName.addressRole.$untouched = !self.formName.addressRole.$touched;
+
+            self.onUpdate({$event: {roles: self.roleModel}});
+
+        }
 
     }
 })();
