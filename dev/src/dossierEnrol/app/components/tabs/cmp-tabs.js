1a244a3bf64a0e2b79bfc87a1ef3ebb66c6c39a5
achtatou <achtatou@softpoint.ca>
2016-08-24 14:58:47 -0400

    Dossier: UI Building (WIP) -Fixed an issue with Wet tabs : ng-click event was broken

--- dev/src/dossierEnrol/app/components/tabs/cmp-tabs.js
+++ dev/src/dossierEnrol/app/components/tabs/cmp-tabs.js
@@ -14,7 +14,7 @@
     angular.module('tabsModule').component('cmpTabs', {
         templateUrl: './components/tabs/tpl-tabs.html',
         controller: tabsCtrl,
-        controllerAs: 'tabs',
+        controllerAs: 'tabsCtrl',
         bindings: {
         }
     });
@@ -23,6 +23,39 @@
 
 
     function tabsCtrl($scope) {
+
+        var self = this;
+
+        self.$onInit = function(){
+           self.tabs = [
+               {
+                   label:"Formulations",
+                   selected:false
+               },
+               {
+                   label:"Appendix 4",
+                   selected:true
+               }
+           ];
+        }
+
+        self.selectTab = function(idx){
+
+            angular.forEach(self.tabs, function(tab){
+              //  console.log('tabsModule item: ' + tab.toSource());
+                tab.selected = false;
+            });
+
+           /* for(var item in self.tabs){
+
+                console.log('tabsModule item: ' + item);
+
+              //  item.selected = false;
+            }*/
+
+            self.tabs[idx].selected = true;
+
+        }
     }
 
 })();
