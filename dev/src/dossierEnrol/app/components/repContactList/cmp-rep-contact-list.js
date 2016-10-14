5dc808c508cdd3476964a56fde076ad0ed2a4166
Dan Kilty <dan.kilty@canada.ca>
2016-10-14 12:02:31 -0400

    Extra error check for the model

--- dev/src/dossierEnrol/app/components/repContactList/cmp-rep-contact-list.js
+++ dev/src/dossierEnrol/app/components/repContactList/cmp-rep-contact-list.js
@@ -31,39 +31,37 @@
     contactListCtrl.$inject = ['$filter','RepContactService']
     function contactListCtrl($filter, RepContactService) {
         var vm = this;
+        vm.selectRecord = -1; //the record to select
+        vm.isDetailValid = true; //used to track if details valid. If they are  not do not allow expander collapse
+        vm.contactList = [];
+        vm.oneRecord = ""; //using required as the validaiton
+        vm.isParentDirty = false; //tracks whether the parent form has been dirtied
+        vm.formAmend = false; //
+        var repContactService=new RepContactService();
+        vm.columnDef = [
+            {
+                label: "FIRST_NAME",
+                binding: "givenName",
+                width: "40"
+            },
+
+            {
+                label: "LAST_NAME",
+                binding: "surname",
+                width: "40"
+            },
+            {
+                label: "ONE_ROLE",
+                binding: "repRole",
+                width: "20"
+            }
+        ]
         /**
          * using to get contact list
          */
         vm.$onInit = function () {
             vm.focused = false;
-
-
-            vm.selectRecord = -1; //the record to select
-            vm.isDetailValid = true; //used to track if details valid. If they are  not do not allow expander collapse
-            vm.contactList = [];
-            vm.oneRecord = ""; //using required as the validaiton
-            vm.isParentDirty = false; //tracks whether the parent form has been dirtied
-            vm.formAmend = false; //
-            var repContactService=new RepContactService();
-            vm.columnDef = [
-                {
-                    label: "FIRST_NAME",
-                    binding: "givenName",
-                    width: "40"
-                },
-
-                {
-                    label: "LAST_NAME",
-                    binding: "surname",
-                    width: "40"
-                },
-                {
-                    label: "ONE_ROLE",
-                    binding: "repRole",
-                    width: "20"
-                }
-            ]
-            // vm.contactList = vm.contacts; //HERE Is how it is bound
+            // vm.contactList = vm.contacts;
         }
         vm.$onChanges = function (changes) {
             if (changes.contacts) {
@@ -145,9 +143,7 @@
         }
 
         vm.disableAdd = function () {
-            if(!vm.contactList)
-                return false;
-
+            if(!vm.contactList) return false;
             return !(vm.isDetailValid && vm.contactList.length < 2);
         }
 
