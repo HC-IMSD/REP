34aecf636f786caf61732a1a412931e02fbac461
Dan Kilty <dan.kilty@canada.ca>
2017-02-15 16:14:24 -0500

    Related Activity Definitions for tests

--- dev/src/forms_R1/app/spec/e2e/component-definitions/def-cmp-related-activity.js
+++ dev/src/forms_R1/app/spec/e2e/component-definitions/def-cmp-related-activity.js
@@ -13,41 +13,66 @@ var RelatedActivity=function(){
     var _brandNameText=element(by.model("$ctrl.productModel.brandName"));
     var _activeNameModel="$ctrl.productModel.ingLabel";
 
+    var _strengthNumber=element(by.model("$ctrl.productModel.strengths"));
+    var _unitsModelString="$ctrl.productModel.units";
+    var _unitsUiSelect=element(by.model(_unitsModelString));
+    var _perText=element(by.model("$ctrl.productModel.per"));
+    var _dosageFormModelString="$ctrl.productModel.dosageForm";
+    var _dosageFormUiSelect=element(by.model(_dosageFormModelString));
+    var _companyNameText=element(by.model("$ctrl.productModel.companyName"));
 
+    /**
+     * Sets the active name by typing some text, then selecting from the lookup
+     * @param value
+     * @param selectionValue
+     */
     this.setActiveNameLookup= function (value,selectionValue) {
         browser.selectTypeAheadPopupValue(_activeNameModel,value,selectionValue)
 
     };
+    this.setBrandNameValue=function(value){
+        _brandNameText.sendKeys(value);
 
+    };
+    this.setStrengthValue=function(value){
+        _strengthNumber.sendKeys(value);
 
-    this.addReferenceProduct=function(){
-        _addRefProductButton.sendKeys(protractor.Key.ENTER);
+    };
+    this.setUnitsTextValue=function(value) {
+        browser.pickUISelectOption(_unitsModelString, value);
     };
 
- /*   var _applStatus = element(by.model("infoCtrl.record.applicationType"));
-    var _versionNumber=element(by.model("infoCtrl.infoModel.enrolmentVersion"));
-    var _dateSaved=element(by.model("infoCtrl.infoModel.dateSaved"));
-    var _dossierId=element(by.model("infoCtrl.record[infoCtrl.tagName]"));
-    var _amendButton = element(by.id("amendSub"));*/
-
-
-    /*this.getApplStatus = function () {
-        return _applStatus.getAttribute('value');
+    this.setPerValue=function(value){
+        _perText.sendKeys(value);
+    };
+    this.setDosageFormTextValue=function(value) {
+        browser.pickUISelectOption(_dosageFormModelString, value);
     };
 
-    this.getVersionNumber = function () {
-        return _versionNumber.getAttribute('value');
+    //======================== Getters
+
+    this.getActiveNameLookup= function () {
+        var _element=element.all(by.model(modelString)).last(); //temporary till a better fix
+        return _element.getAttribute('value');
     };
-    this.getDateSaved = function () {
-        return _dateSaved.getAttribute('value');
+    this.getBrandNameValue=function(){
+        return _brandNameText.getAttribute('value');
     };
-    this.getDossierId = function () {
-        return _dossierId.getAttribute('value');
+    this.getStrengthValue=function(){
+        return _strengthNumber.getAttribute('value');
     };
 
-    this.clickAmend=function(){
-        _amendButton.sendKeys(protractor.Key.ENTER);
-    };*/
+    this.getUnitsTextValue=function(value) {
+        return browser.getUISelectModelValue(_unitsUiSelect, _unitsModelString);
+    };
+
+
+    this.getPerValue=function(){
+        return _perText.getAttribute('value');
+    };
+    this.getDosageFormTextValue=function(value) {
+        return browser.getUISelectModelValue(_dosageFormUiSelect, _dosageFormModelString);
+    };
 
 };
 
