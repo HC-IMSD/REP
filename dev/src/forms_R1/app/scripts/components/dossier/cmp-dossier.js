1a8a47d31b909832e7db073f8d6fe3f5a02d59e8
Dan Kilty <dan.kilty@canada.ca>
2017-02-17 16:53:48 -0500

    WIP Dossier instructions

--- dev/src/forms_R1/app/scripts/components/dossier/cmp-dossier.js
+++ dev/src/forms_R1/app/scripts/components/dossier/cmp-dossier.js
@@ -78,30 +78,9 @@
         self.extraAppendix = [];
         self.noThera = "";
         self.oneRefSelected = "";
-        self.alert1 = {
-            type: 'info',
-            msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat nunc et tempor malesuada. Nullam tristique ligula blandit, posuere est ac, sagittis mi. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras ullamcorper sagittis erat ac lobortis. Suspendisse bibendum sed mauris eget condimentum. Suspendisse egestas ligula a libero tincidunt, ut vehicula sem fermentum. Quisque semper scelerisque urna, in dignissim odio condimentum ac. Nullam suscipit malesuada magna, eget lacinia nulla tempor id. Curabitur tristique ipsum libero, ut pulvinar ipsum venenatis non. Ut porta, sem non blandit aliquet, ante mauris porta ex, quis iaculis elit orci eu leo. Morbi at enim nec odio ullamcorper molestie. Nulla sit amet magna consequat, blandit orci a, porta eros. Sed enim nisl, tempus ac imperdiet a, ornare gravida sapien. Curabitur ultricies dolor aliquet bibendum accumsan.',
-            show: false
-        };
-        self.alert2 = {
-            type: 'info',
-            msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat nunc et tempor malesuada. Nullam tristique ligula blandit, posuere est ac, sagittis mi. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras ullamcorper sagittis erat ac lobortis. Suspendisse bibendum sed mauris eget condimentum. Suspendisse egestas ligula a libero tincidunt, ut vehicula sem fermentum. Quisque semper scelerisque urna, in dignissim odio condimentum ac. Nullam suscipit malesuada magna, eget lacinia nulla tempor id. Curabitur tristique ipsum libero, ut pulvinar ipsum venenatis non. Ut porta, sem non blandit aliquet, ante mauris porta ex, quis iaculis elit orci eu leo. Morbi at enim nec odio ullamcorper molestie. Nulla sit amet magna consequat, blandit orci a, porta eros. Sed enim nisl, tempus ac imperdiet a, ornare gravida sapien. Curabitur ultricies dolor aliquet bibendum accumsan.',
-            show: false
-        };
-        self.alert3 = {
-            type: 'info',
-            msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat nunc et tempor malesuada. Nullam tristique ligula blandit, posuere est ac, sagittis mi. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras ullamcorper sagittis erat ac lobortis. Suspendisse bibendum sed mauris eget condimentum. Suspendisse egestas ligula a libero tincidunt, ut vehicula sem fermentum. Quisque semper scelerisque urna, in dignissim odio condimentum ac. Nullam suscipit malesuada magna, eget lacinia nulla tempor id. Curabitur tristique ipsum libero, ut pulvinar ipsum venenatis non. Ut porta, sem non blandit aliquet, ante mauris porta ex, quis iaculis elit orci eu leo. Morbi at enim nec odio ullamcorper molestie. Nulla sit amet magna consequat, blandit orci a, porta eros. Sed enim nisl, tempus ac imperdiet a, ornare gravida sapien. Curabitur ultricies dolor aliquet bibendum accumsan.',
-            show: false
-        };
-        self.alert4 = {
-            type: 'info',
-            msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat nunc et tempor malesuada. Nullam tristique ligula blandit, posuere est ac, sagittis mi. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras ullamcorper sagittis erat ac lobortis. Suspendisse bibendum sed mauris eget condimentum. Suspendisse egestas ligula a libero tincidunt, ut vehicula sem fermentum. Quisque semper scelerisque urna, in dignissim odio condimentum ac. Nullam suscipit malesuada magna, eget lacinia nulla tempor id. Curabitur tristique ipsum libero, ut pulvinar ipsum venenatis non. Ut porta, sem non blandit aliquet, ante mauris porta ex, quis iaculis elit orci eu leo. Morbi at enim nec odio ullamcorper molestie. Nulla sit amet magna consequat, blandit orci a, porta eros. Sed enim nisl, tempus ac imperdiet a, ornare gravida sapien. Curabitur ultricies dolor aliquet bibendum accumsan.',
-            show: false
-        };
-
+        self.alerts = [false, false, false, false];
 
         var yesValue = YES;
-
         self.$onInit = function () {
             self.dossierService = new DossierService();
 
@@ -362,40 +341,25 @@
             return false;
         };
 
-        self.closeAlert = function (value) {
-            switch (value) {
-                case '1':
-                    self.alert1.show = false;
-                    break;
-                case '2':
-                    self.alert2.show = false;
-                    break;
-                case '3':
-                    self.alert3.show = false;
-                    break;
-                case '4':
-                    self.alert4.show = false;
-                    break;
-            }
-        };
-
         self.addInstruct = function (value) {
 
-            switch (value) {
-                case '1':
-                    self.alert1.show = true;
-                    break;
-                case '2':
-                    self.alert2.show = true;
-                    break;
-                case '3':
-                    self.alert3.show = true;
-                    break;
-                case '4':
-                    self.alert4.show = true;
-                    break;
+            if (angular.isUndefined(value)) return;
+            if (value < self.alerts.length) {
+                self.alerts[value] = true;
             }
         }
+
+
+        /**
+         * Closes the instruction alerts
+         * @param value
+         */
+        self.closeAlert = function (value) {
+            if (angular.isUndefined(value)) return;
+            if (value < self.alerts.length) {
+                self.alerts[value] = false;
+            }
+        };
 
 
     }
