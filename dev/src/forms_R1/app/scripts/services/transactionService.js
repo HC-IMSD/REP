8808c473bfd3a179ced1c5e2822ad6250239c68b
stevezhaoca <steve.zhao@canada.ca>
2018-02-19 15:14:50 -0500

    REP-28, RT form - Chnage Lifecycle Rec associations of Sequence Clean-up and Notification of interruption of sale

--- dev/src/forms_R1/app/scripts/services/transactionService.js
+++ dev/src/forms_R1/app/scripts/services/transactionService.js
@@ -20,6 +20,7 @@
 
     //version 1.1 bug fix?
     //version 1.2 added Submission package/rq to MPNC, MPDNS
+    //version 1.3 Chnage Lifecycle Rec associations of Sequence Clean-up and Notification of interruption of sale
 
     function TransactionService($filter, getCountryAndProvinces, getContactLists, TransactionLists, YES, NO) {
         function TransactionService() {
@@ -30,7 +31,7 @@
                 // enrolmentVersion: "0.0",
                 dateSaved: "",
                 //applicationType: "NEW",
-                softwareVersion: "1.2.0",
+                softwareVersion: "1.3.0",
                 isEctd: "Y",
                 ectd: {
                     companyId: "",
@@ -88,7 +89,7 @@
                     TRANSACTION_ENROL: {
                         template_type: "PHARMA",
                         date_saved: today,
-                        software_version: "1.1.0",
+                        software_version: "1.3.0",
                         data_checksum: jsonObj.dataChecksum,
                         is_ectd: jsonObj.isEctd
                     }
