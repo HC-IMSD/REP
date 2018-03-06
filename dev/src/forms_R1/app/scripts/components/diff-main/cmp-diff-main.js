a2f2771c047e411f4290735c31d13ad6b021a7f8
Dan Kilty <dan.kilty@canada.ca>
2018-03-06 11:59:00 -0500

    Library for external web page launch. Demo in diff page

--- dev/src/forms_R1/app/scripts/components/diff-main/cmp-diff-main.js
+++ dev/src/forms_R1/app/scripts/components/diff-main/cmp-diff-main.js
@@ -1,21 +1,21 @@
+"use strict";
 /**
  * Created by dkilty on 8/26/2016.
  */
 (function () {
-    'use strict';
+
     angular
         .module('diffMain', [
             'fileIODiff',
             'ui.tree',
-            'diffModule'
+            'diffModule',
+            'repCommon'
         ])
 })();
 
 // https://github.com/angular-ui-tree/angular-ui-tree
 
-
 (function () {
-    'use strict';
     angular
         .module('diffMain')
         .component('cmpDiffMain', {
@@ -26,8 +26,8 @@
             }
         });
 
-    MainController.$inject = ['$translate', '$filter','$scope','diffEngine','$window'];
-    function MainController($translate, $filter,$scope,diffEngine,$window) {
+    MainController.$inject = ['$translate', '$filter','$scope','diffEngine','repUtil'];
+    function MainController($translate, $filter,$scope,diffEngine,repUtil) {
         var vm = this;
         vm.version=0.2; //version number of the form
 
@@ -96,16 +96,16 @@
 
         vm.showRaw=function(){
           vm.isRaw=!vm.isRaw;
-
         };
 
          vm.test=function(){
-            $window.open('https://www.canada.ca/en/health-canada/services/drugs-health-products/drug-products/fees/fees-review-drug-submissions-applications.html#a2323');
-
+           // $window.open('https://www.canada.ca/en/health-canada/services/drugs-health-products/drug-products/fees/fees-review-drug-submissions-applications.html#a2323');
+             repUtil.openExternalLink('https://www.canada.ca/en/health-canada/services/drugs-health-products/drug-products/fees/fees-review-drug-submissions-applications.html#a2323',null);
 
         }
         vm.testfr=function(){
-            $window.open('https://www.canada.ca/fr/sante-canada/services/medicaments-produits-sante/medicaments/frais/examen-presentations-demandes-drogue.html#a2.3.2.3');
+             repUtil.openInternalLink('help/help-activity-load-en.html',null)
+           // $window.open('https://www.canada.ca/fr/sante-canada/services/medicaments-produits-sante/medicaments/frais/examen-presentations-demandes-drogue.html#a2.3.2.3');
         }
 
 
