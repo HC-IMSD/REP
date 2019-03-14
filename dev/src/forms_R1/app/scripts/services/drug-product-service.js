eb95424cc26e78970a79c77f53d420e4cc6dc021
IanYangCa <IanYang.ca@gmail.com>
2019-03-14 11:07:14 -0400

    Fixed load XML data file, it does not initial units and units of measure

--- dev/src/forms_R1/app/scripts/services/drug-product-service.js
+++ dev/src/forms_R1/app/scripts/services/drug-product-service.js
@@ -543,6 +543,7 @@
                         data1: Number(item.strength.data1),
                         data2: Number(item.strength.data2) },
                     "units": "",
+                    "unitsHtml": "",
                     "otherUnits": item.units_other,
                     "per": item.per,
                     "perPresentationValue": Number(1),
@@ -550,6 +551,7 @@
                     "perPresUnits": "",
                     "perPresOtherUnits": "",
                     "perMeasUnits": "",
+                    "perMeasUnitsHtml": "",
                     "perMeasOtherUnits": "",
                     "calcAsBase": item.is_base_calc,
                     "isNano": item.is_nanomaterial,
@@ -569,6 +571,7 @@
                         unitsValue = item.units.__text;
                     }
                     obj.units = $filter('findListItemById')(DossierLists.getUnitsList(), {id: unitsValue});
+                    obj.unitsHtml = obj.units[$translate.proposedLanguage() || $translate.use()];
                 }
 
                 if (item.per) {
@@ -595,6 +598,7 @@
                         unitsValue = item.per_units.__text;
                     }
                     obj.perMeasUnits = $filter('findListItemById')(DossierLists.getUnitsMeasureList(), {id: unitsValue});
+                    obj.perMeasUnitsHtml = obj.perMeasUnits[$translate.proposedLanguage() || $translate.use()];
                     obj.perMeasOtherUnits = item.per_units_other_details;
                 }
 
